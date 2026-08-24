3b7f81103cf6de1bb861777255c301c3904d4889
Celia <celiamen92240@gmail.com>
2026-08-24 13:18:33 +0200

    Fix: Add ShieldCheck import to completely resolve white screen

--- frontend/src/views/HomeView.jsx
+++ frontend/src/views/HomeView.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import {
   Heart, Sparkles, Trophy, MessageSquareText,
-  CalendarHeart, ArrowRight, Lightbulb, ChevronRight, Lock, Mail, Sprout, Camera, MailOpen, Puzzle
+  CalendarHeart, ArrowRight, Lightbulb, ChevronRight, Lock, Mail, Sprout, Camera, MailOpen, Puzzle, ShieldCheck
 } from 'lucide-react';
 import BabyVectorLogo from '../components/BabyVectorLogo';
 import { fruitsData } from '../data/fruitsData';
